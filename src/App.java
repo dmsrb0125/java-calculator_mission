e4a5de6ad457c635acacfeef64051c4b4ef1f27d
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-04-29 15:30:03 +0900

    refactor: Lever1-요구사항1번 유효성검사 추가 및 수정

--- src/App.java
+++ src/App.java
@@ -3,21 +3,41 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Scanner를 사용하여 사용자로부터 첫번째, 두번째 입력을 받고 입력값을  getNonNegativeIntege를 통해 유효성검사 실시
+        // getNonNegativeInteger를 통해 Scanner를 사용하여 사용자로부터 첫번째, 두번째 입력을 받고 입력값 유효성 검사 실시
         int firstNumber = getNonNegativeInteger(scanner, "첫 번째 숫자를 입력해주세요(양의 정수 또는 0만 입력 가능합니다): ");
         int secondNumber = getNonNegativeInteger(scanner, "두 번째 숫자를 입력해주세요(양의 정수 또는 0만 입력 가능합니다): ");
-        scanner.close();
+
     }
 
-    // 음수 입력을 확인하는 메서드
+    // 입력값 유효성검사후 반환하는 메서드
     private static int getNonNegativeInteger(Scanner scanner, String prompt) {
-        System.out.print(prompt);
-        int number = scanner.nextInt();
-        while (number < 0) {
-            System.out.print("양의 정수 또는 0만 입력 가능합니다. 다시 입력해주세요:");
-            number = scanner.nextInt();
+        while (true) {
+            System.out.print(prompt);
+            String input = scanner.nextLine().trim();
+
+            // 입력값이 없을 때 입력 다시받기
+            if (input.isEmpty()) {
+                System.out.println("입력이 없습니다. 숫자를 입력해주세요.");
+                continue;
+            }
+
+            try {
+                int number = Integer.parseInt(input);
+
+                // 음수일 때 입력 다시받기
+                if (number < 0) {
+                    System.out.println("양의 정수 또는 0만 입력 가능합니다. 다시 입력해주세요:");
+                    continue;
+                }
+
+                // 양의 정수 또는 0일 경우 반복문 탈출
+                return number;
+            } catch (NumberFormatException e) {
+                System.out.println("올바르지 않는 입력입니다. 다시 입력해주세요:");
+            }
         }
-        return number;
     }
+
+
+
 }
