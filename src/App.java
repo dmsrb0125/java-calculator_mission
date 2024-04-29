ed041653fe6a3cc034cce1d220907ecc4b705a83
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-04-29 13:41:18 +0900

    refacotr: Lever1-요구사항1번 완료

--- src/App.java
+++ src/App.java
@@ -4,25 +4,20 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Scanner를 사용하여 사용자로부터 첫번째 입력 받기
-        System.out.print("첫 번째 숫자를 입력해주세요(양의 정수 또는 0만 입력 가능합니다): ");
-        int firstNumber = scanner.nextInt();
+        // Scanner를 사용하여 사용자로부터 첫번째, 두번째 입력을 받고 입력값을  getNonNegativeIntege를 통해 유효성검사 실시
+        int firstNumber = getNonNegativeInteger(scanner, "첫 번째 숫자를 입력해주세요(양의 정수 또는 0만 입력 가능합니다): ");
+        int secondNumber = getNonNegativeInteger(scanner, "두 번째 숫자를 입력해주세요(양의 정수 또는 0만 입력 가능합니다): ");
+        scanner.close();
+    }
 
-        // 음수 입력을 확인하고 재요청
-        while (firstNumber < 0) {
-            System.out.println("양의 정수 또는 0만 입력 가능합니다. 다시 입력해주세요:");
-            firstNumber = scanner.nextInt();
+    // 음수 입력을 확인하는 메서드
+    private static int getNonNegativeInteger(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        int number = scanner.nextInt();
+        while (number < 0) {
+            System.out.print("양의 정수 또는 0만 입력 가능합니다. 다시 입력해주세요:");
+            number = scanner.nextInt();
         }
-
-        // Scanner를 사용하여 사용자로부터 두번째 입력 받기
-        System.out.print("두 번째 숫자를 입력해주세요(양의 정수 또는 0만 입력 가능합니다) ");
-        int secondNumber = scanner.nextInt();
-
-        // 음수 입력을 확인하고 재요청
-        while (secondNumber < 0) {
-            System.out.println("양의 정수 또는 0만 입력 가능합니다. 다시 입력해주세요:");
-            secondNumber= scanner.nextInt();
-        }
-
+        return number;
     }
 }
