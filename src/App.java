091cb78a64c55b95c4057e5b8c6240e1b21862c7
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-04-29 19:56:21 +0900

    feat: Level1-요구사항8번

--- src/App.java
+++ src/App.java
@@ -15,6 +15,7 @@ public class App {
             double result = calculate(firstNumber,secondNumber,applyOperator);
             System.out.println("결과: " + result);
 
+            resultArray.add(result);
             // remove 입력시  가장 먼저 저장된 결과가 삭제, 다른입력시 넘어감
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제 / 다른키 누를시 넘어감)");
             String removeInput= scanner.nextLine();
@@ -22,17 +23,25 @@ public class App {
                 resultArray.remove(0);
             }
 
+            // inquiry 입력시 저장된 연산 결과 전부를 출력, 다른입력시 넘어감
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회 / 다른키 누를시 넘어감)");
+            String getInput= scanner.nextLine();
+            if ("inquiry".equals(getInput)) {
+                for(double i: resultArray){
+                    System.out.println(i);
+                }
+            }
+
+
             // exit 입력 시 종료, 다른입력시 계산 무한 반복진행
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료 /  다른키 누를시 다음 계산 진행)");
             String continueInput= scanner.nextLine();
             if ("exit".equals(continueInput)) {
                 break;
             }
-            resultArray.add(result);
 
-            for(double i: resultArray){
-                System.out.println(i);
-            }
+
+
 
         }
         scanner.close();
